f129f9da79918121576400b5e0f30ba0b4c2d7bd
Vinikra <vinikra14@gmail.com>
2025-03-14 10:18:02 -0400

    Add the middleware

---
+++ src/middleware.rs
@@ -0,0 +1,22 @@
+use axum::{http::{Request, header}, middleware::Next, response::Response};
+use crate::auth::verify_token;
+
+pub async fn auth_middleware<B>(
+    req: Request<B>,
+    next: Next<B>,
+) -> Result<Response, axum::http::StatusCode> {
+    let auth_header = req.headers()
+    .get(header::AUTHORIZATION)
+    .and_then(|h| h.to_str().ok());
+
+    if let Some(header) = auth_header {
+        if let Some(token) = header.strip_prefix("Bearer ") {
+            if let Some(user_id) = verify_token(token) {
+                let mut req = req;
+                req.extensions_mut().insert(user_id);
+                return Ok(next.run(req).await);
+            }
+        }
+    }
+    Err(axum::http:StatusCode::UNAUTHORIZED)
+}
